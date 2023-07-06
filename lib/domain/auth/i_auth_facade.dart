bf26fa479f7fbbab1d17cff19df2e9bf08207a5a
SalmanNaghori <69813706+SalmanNaghori@users.noreply.github.com>
2023-07-06 14:34:01 +0530

    changes_in_bloc

--- lib/domain/auth/i_auth_facade.dart
+++ lib/domain/auth/i_auth_facade.dart
@@ -1,9 +1,10 @@
 import 'package:dartz/dartz.dart';
 import 'package:demo_of_ddd/domain/auth/auth_faild.dart';
-
+import 'package:demo_of_ddd/domain/auth/user.dart';
 import 'value_objects.dart';
 
 abstract class IAuthFacade {
+  Future<Option<User>> getSignedInUser();
   Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
     required EmailAddress emailAddress,
     required Password password,
@@ -13,4 +14,5 @@ abstract class IAuthFacade {
     required Password password,
   });
   Future<Either<AuthFailure, Unit>> signInWithGoogle();
+  Future<void> signOut();
 }
